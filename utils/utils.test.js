0ea61270f1436502a312524042242b557296e7ed
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-18 17:26:54 -0500

    used the expect assertion library to test common issues

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,16 +1,51 @@
+const expect = require('expect');
+
 const utils = require('./utils')
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}`);        
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(5);
-    if (res !== 25) {
-        throw new Error(`Expected 25, but got ${res}`)
-    }
+    expect(res).toBe(25).toBeA('number');
 });
 
+it('should expect some value not equal another value', () => {
+    expect(12).toNotBe(11); //checks equality for strings and numbers
+});
+
+it('should expect two objects(arrays) to be equal', () => {
+    expect({name: 'Daniel'}).toEqual({name: 'Daniel'}); //checks equality for objects and array
+});
+
+it('should expect an array to have a value', () => {
+    expect([2,3,4]).toInclude(3); //checks if an array includes some value
+})
+
+it('should expect an array to not have a value', () => {
+    expect([2,3,4]).toExclude(5);
+});
+
+it('should expect an object to have a particular property value', () => {
+    expect({
+        name: 'Daniel',
+        age: 23,
+        location: 'Houston'
+    }).toInclude({
+        age: 23
+    })
+});
+
+it('should verify first and last names are set', () => {
+    var user = {
+        location: 'Houston',
+        age: 23
+    };
+    var res = utils.setName(user, 'Daniel Hernandez');
+    expect(res).toInclude({
+        firstName: 'Daniel',
+        lastName: 'Hernandez'
+    }).toBeA('object');
+});
