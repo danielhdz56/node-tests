419f4e3a533f554685f4850417c6d62a2fbe8881
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-18 18:36:04 -0500

    organized similar tests using describe

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.send([{
+            name: 'Daniel',
+            age: 23
+        },
+        {
+            name: 'Armand',
+            age: 25
+        }
+    ]);
+});
+
+app.listen(3000, () => console.log('Server is up and running'));
+module.exports.app = app;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,64 +2,75 @@ const expect = require('expect');
 
 const utils = require('./utils')
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
-    expect(res).toBe(44).toBeA('number');
-});
+describe('Utils', () => {
 
-it('should async add two numbers', (done) => { // use done as an argument to let mocha know to wait until done is called to test the async code
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+            expect(res).toBe(44).toBeA('number');
+        });
+
+        it('should async add two numbers', (done) => { // use done as an argument to let mocha know to wait until done is called to test the async code
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
     });
-});
+    describe('#square', () => {
+        it('should square a number', () => {
+            var res = utils.square(5);
+            expect(res).toBe(25).toBeA('number');
+        });
 
-it('should square a number', () => {
-    var res = utils.square(5);
-    expect(res).toBe(25).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(5, (square) => {
-        expect(square).toBe(25).toBeA('number');
-        done();
-    });
-})
-
-it('should expect some value not equal another value', () => {
-    expect(12).toNotBe(11); //checks equality for strings and numbers
-});
-
-it('should expect two objects(arrays) to be equal', () => {
-    expect({name: 'Daniel'}).toEqual({name: 'Daniel'}); //checks equality for objects and array
-});
-
-it('should expect an array to have a value', () => {
-    expect([2,3,4]).toInclude(3); //checks if an array includes some value
-})
-
-it('should expect an array to not have a value', () => {
-    expect([2,3,4]).toExclude(5);
-});
-
-it('should expect an object to have a particular property value', () => {
-    expect({
-        name: 'Daniel',
-        age: 23,
-        location: 'Houston'
-    }).toInclude({
-        age: 23
+        it('should async square a number', (done) => {
+            utils.asyncSquare(5, (square) => {
+                expect(square).toBe(25).toBeA('number');
+                done();
+            });
+        });
     })
-});
 
-it('should verify first and last names are set', () => {
-    var user = {
-        location: 'Houston',
-        age: 23
-    };
-    var res = utils.setName(user, 'Daniel Hernandez');
-    expect(res).toInclude({
-        firstName: 'Daniel',
-        lastName: 'Hernandez'
-    }).toBeA('object');
-});
+    it('should expect some value not equal another value', () => {
+        expect(12).toNotBe(11); //checks equality for strings and numbers
+    });
+
+    it('should expect two objects(arrays) to be equal', () => {
+        expect({
+            name: 'Daniel'
+        }).toEqual({
+            name: 'Daniel'
+        }); //checks equality for objects and array
+    });
+
+    it('should expect an array to have a value', () => {
+        expect([2, 3, 4]).toInclude(3); //checks if an array includes some value
+    })
+
+    it('should expect an array to not have a value', () => {
+        expect([2, 3, 4]).toExclude(5);
+    });
+
+    it('should expect an object to have a particular property value', () => {
+        expect({
+            name: 'Daniel',
+            age: 23,
+            location: 'Houston'
+        }).toInclude({
+            age: 23
+        })
+    });
+
+    it('should verify first and last names are set', () => {
+        var user = {
+            location: 'Houston',
+            age: 23
+        };
+        var res = utils.setName(user, 'Daniel Hernandez');
+        expect(res).toInclude({
+            firstName: 'Daniel',
+            lastName: 'Hernandez'
+        }).toBeA('object');
+    });
+
+})
